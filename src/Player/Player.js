ea9c646884d7245e2dbf9c3d6ed5851a9f7dc351
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2022-11-11 18:18:02 +0530

    Player styling

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,10 +1,8 @@
-import { ImVolumeLow } from "react-icons/im";
-import { ImVolumeMedium } from "react-icons/im";
-import { ImVolumeHigh } from "react-icons/im";
-import { ImVolumeMute2 } from "react-icons/im";
+
 import { IoPlaySkipBack } from "react-icons/io5";
 import { IoPlaySkipForward } from "react-icons/io5";
 import { IoPlay } from "react-icons/io5";
+import { GiPauseButton } from "react-icons/gi";
 import VolumeSlider from "../VolumeSlider/VolumeSlider";
 import "./player.css";
 import { useState } from "react";
@@ -30,20 +28,23 @@ export default function Player() {
           <div class="track-frame">
             <div class="track-photo"></div>
             <span class="track-name">Till I Collapse</span>
-            <div class="track-progress">
-              <span class="track-elapsed">00:00</span>
-              <input class="track-slider" type="range" min="0" max="100" />
-              <span class="track-length">11:11</span>
+              <div class="track-slider">
+                <input class="track-slider" type="range" min="0" max="100" />
+                <progress class="track-progress" min="0" max="100" value="50"></progress>
+              
+              
             </div>
+            <span class="track-elapsed">00:00</span>
+              <span class="track-length">11:11</span>
             <div class="track-play-control">
-              <span class="play-previous">
-                <IoPlaySkipBack />
+              <span class="play-previous play-btns">
+                <IoPlaySkipBack class="play-icon" id="prev" />
               </span>
-              <span class="play-stop">
-                <IoPlay />
+              <span class="play-stop  play-btns">
+                <GiPauseButton class="play-icon" id="playstop" />
               </span>
-              <span class="play-next">
-                <IoPlaySkipForward />
+              <span class="play-next play-btns">
+                <IoPlaySkipForward class="play-icon" id="next" />
               </span>
             </div>
           </div>

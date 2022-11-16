aa59d1d18d7c46a31d262e08eeae57043c39997d
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2022-11-16 19:21:34 +0530

    Sidebar Redesigned

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import Login from "./Login/Login"
-import Player from "./Player/Player";
 import TrackSlider from "./Player/TrackSlider";
 import SignUp from './Sign Up/SignUp';
+import styled from "styled-components";
+import Player2 from "./SongPlayer/Player";
 export default function App() {
   
   return (
     <>
-    <SignUp/>
+    <Sidebar/>
     </>
   );
 }
+
---
+++ src/SongPlayer/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Player(){
+    return(
+        <>
+            <div class = "player-container">
+                <div class = "like-icon"></div>
+                <div class = "track-image"></div>
+                
+            </div>
+        </>
+    );
+}

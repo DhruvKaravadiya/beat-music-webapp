e853f2a79bca0a2df9b02996ee5e545a9da05dc9
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2022-11-16 11:07:08 +0530

    signUp page design completed

--- src/Sign Up/SignUp.js
+++ src/Sign Up/SignUp.js
@@ -4,50 +4,49 @@ export default function signUp() {
     <>
       <div class="signUp-page-body">
         <div class="signUp-card">
-          <div class ="sig"></div>
-          <p class="signUp-card-header ">
-            <h1>SIGN UP</h1>
-          </p>
+          <div class="signUp-head-logo"></div>
+          <div class="signUp-body">
+            <p class="signUp-card-header ">
+              <h1>SIGN UP</h1>
+            </p>
 
-          <form class="signUp-card-form">
-           
-            <div class="signUp-form-item ">
-             
-              <input
-                type="text"
-                placeholder="Enter Username"
-                title="Username must contain alphabets and number"
-                required
-              />
-            </div>
-            <div class="signUp-form-item ">
-             
-              <input
-                id="email"
-                type="text"
-                name="email"
-                pattern="[a-zA-Z]{3,}@[a-zA-Z]{3,}[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,}"
-                required
-                placeholder="Enter you Email"
-              />
-            </div>
+            <form class="signUp-card-form">
+              <div class="signUp-form-item ">
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  title="Username must contain alphabets and number"
+                  required
+                />
+              </div>
+              <div class="signUp-form-item ">
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  pattern="[a-zA-Z]{3,}@[a-zA-Z]{3,}[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,}"
+                  required
+                  placeholder="Enter you Email"
+                />
+              </div>
 
-            <div class="signUp-form-item ">
-            
-              <input type="password" placeholder="Enter Password" required />
+              <div class="signUp-form-item ">
+                <input type="password" placeholder="Enter Password" required />
+              </div>
+              <div class="signUp-form-item ">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  required
+                />
+              </div>
+              <div class="signUp-form-item signUp-btn">
+                <button type="submit "> Sign Up</button>
+              </div>
+            </form>
+            <div class="signUp-card-footer ">
+              Already have and account?<a class="Login">Login</a>
             </div>
-            <div class="signUp-form-item ">
-              <input type="password" placeholder="Confirm Password" required />
-            </div>
-            <div class="signUp-form-item ">
-              <button type="submit " >
-                {" "}
-                Sign Up
-              </button>
-            </div>
-          </form>
-          <div class="signUp-card-footer ">
-            Already have and account?<a>Login</a>
           </div>
         </div>
       </div>

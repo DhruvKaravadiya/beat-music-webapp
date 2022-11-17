a92dd1cde8f1c421dde08b18ca92f2eb13e0b903
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2022-11-17 19:42:51 +0530

    player design 90%

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
   
   return (
     <>
-    <Sidebar/>
+    <Player2 />
     </>
   );
 }
--- src/SongPlayer/Player.js
+++ src/SongPlayer/Player.js
@@ -1,10 +1,50 @@
 import React from "react";
-export default function Player(){
+import './Player.css';
+import { useState } from "react";
+import { HiOutlineDownload } from 'react-icons/hi';
+import { RiHeartLine  , RiHeartFill} from 'react-icons/ri'
+import { BsSkipBackwardFill, BsSkipForwardFill ,BsPlayFill, BsPauseFill } from 'react-icons/bs';
+import { ImVolumeMute2 , ImVolumeLow , ImVolumeHigh , ImVolumeMedium} from 'react-icons/im';
+import { TbRepeat } from 'react-icons/tb';
+import Eminem from './Eminem.png'; 
+export default function Player(){;
     return(
         <>
             <div class = "player-container">
-                <div class = "like-icon"></div>
-                <div class = "track-image"></div>
+<div class ="first-block">
+                <div class = "like-icon">
+                    <RiHeartLine class= "liked"/>
+                </div>
+                <div class = "track-image">
+                    <img src={Eminem} class = "image" alt=""/>
+                </div>
+                <span class = "track-name">Till I Collapse</span>
+                <div class = "track-slider-block">
+                    <div class = "track-time">
+                        <span class="track-elapsed">0.00</span>
+                        <span class="track-length">3.20</span>
+                    </div>
+                    <div class = "track-slider">
+                            <input type="range" min="0" max="100" />
+                            <progress min="0" max ="100" value="50"/>
+                    </div>
+                    </div></div>
+                    <div class = "track-control">
+                        <div class= "track-icons">
+                        < HiOutlineDownload class="track-control-icon"/>
+                        < BsSkipForwardFill class="track-control-icon"/>
+                        < BsPlayFill class="track-control-icon"/>
+                        < BsSkipBackwardFill class="track-control-icon"/>
+                        < TbRepeat class="track-control-icon"/>
+                        </div>
+                    </div>
+                    <div class = "track-volume-block">
+                        <ImVolumeMedium class="volume-icon"/>
+                        <div class ="volume-slider-block">
+                            <input type="range" min="0" max="100" />
+                            <progress min="0" max ="100" value="50"/>
+                        </div>
+                    </div>
                 
             </div>
         </>

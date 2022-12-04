6083199e0987354c9044f7ba19cc2e09ca4676e3
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2022-12-04 09:41:32 +0530

    Creating  New Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
-import Sidebar from "./Sidebar/Sidebar";
-import Login from "./Login/Login"
-import SignUp from './Sign Up/SignUp';
-import styled from "styled-components";
-import Player from "./SongPlayer/Player";
-import Front from './Front/Front';
-import Playlist from "./SongPlayer/Playlist";
-import Test from "./Text";
+import React from "react";
 export default function App() {
-
   return (
     <>
-    <Sidebar/>
-      <Player/>
     </>
   );
 }
-
--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-
-import App from "./App";
-
+// import Sidebar from "./Sidebar/Sidebar";
+// import Player from "./SongPlayer/Player";
+// import Account from "./SidebarPages/Account";
+// import Liked from "./SidebarPages/Liked";
+// import Create from "./SidebarPages/Create";
+// import Home from "./SidebarPages/Home";
+// import Library from "./SidebarPages/Library";
+// import Search from "./SidebarPages/Search";
+// import Settings from "./SidebarPages/Settings";
+import Layout from "./MainLayout/Layout";
+// import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App />
+  <Layout />
+    {/* <BrowserRouter>
+      <Sidebar/>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="index/search" element={<Search />} />
+        <Route path="index/library" element={<Library />} />
+        <Route path="index/create" element={<Create />} />
+        <Route path="index/liked" element={<Liked />} />
+        <Route path="index/account" element={<Account />} />
+        <Route path="index/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+    <Player  class="i-player"/> */}
+    
   </>
 );
 
